a122905a6b48b18081ae6225d665c17ba4123557
mawqert <mironov.darkhorse2016@yandex.ru>
2025-09-18 20:33:09 +0300

    fixed Primes

--- src/lab1/Primes.java
+++ src/lab1/Primes.java
@@ -12,7 +12,7 @@ public class Primes {
     }
 
     public static boolean isPrime(int n) {
-        for (int i = 2; i < Math.sqrt(n) + 1; i++) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
             }
