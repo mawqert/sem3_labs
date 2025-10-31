06d8097b3403e9f084689c722f32a0982aead5cb
mawqert <mironov.darkhorse2016@yandex.ru>
2025-10-31 13:13:20 +0300

    3 lab(example for library)

--- src/lab3/Library.java
+++ src/lab3/Library.java
@@ -20,6 +20,18 @@ public class Library {
     public void removeBook(String isbn) {
         books.remove(isbn);
     }
-    
 
+    public static void main(String[] args) {
+        Library library = new Library();
+        Book book1 = new Book(10, "title", "author");
+        Book book2 = new Book(10, "title", "author");
+        Book book3 = new Book(10, "title", "author");
+        Book book4 = new Book(10, "title", "author");
+        library.addBook("1111111", book1);
+        library.addBook("2222222", book2);
+        library.addBook("3333333", book3);
+        library.addBook("4444444", book4);
+
+        System.out.println(library.findBook("1111111"));
+    }
 }
